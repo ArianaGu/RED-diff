fix overflow, add extra iter

algos/reddiff.py
@@ -32,6 +32,7 @@ class REDDIFF(DDIM):
 
     def sample(self, x, y, ts, **kwargs):
         y_0 = kwargs["y_0"]
+        num_loop = kwargs["num_loop"]
         sigma_y = self.cfg.algo.sigma_y
         n = x.size(0)
         H = self.H
@@ -52,76 +53,77 @@ class REDDIFF(DDIM):
         optimizer = torch.optim.Adam([mu], lr=self.lr, betas=(0.9, 0.99), weight_decay=0.0)   #original: 0.999
         #optimizer = torch.optim.SGD([mu], lr=1e6, momentum=0.9)  #momentum=0.9
 
-        for ti, si in zip(reversed(ts), reversed(ss)):
-            
-            
-            t = torch.ones(n).to(x.device).long() * ti
-            s = torch.ones(n).to(x.device).long() * si
-            alpha_t = self.diffusion.alpha(t).view(-1, 1, 1, 1)
-            alpha_s = self.diffusion.alpha(s).view(-1, 1, 1, 1)
-            
-            sigma_x0 = self.sigma_x0  #0.0001
-            noise_x0 = torch.randn_like(mu)
-            noise_xt = torch.randn_like(mu)
+        for _ in range(num_loop):
+            for ti, si in zip(reversed(ts), reversed(ss)):
+                
+                
+                t = torch.ones(n).to(x.device).long() * ti
+                s = torch.ones(n).to(x.device).long() * si
+                alpha_t = self.diffusion.alpha(t).view(-1, 1, 1, 1)
+                alpha_s = self.diffusion.alpha(s).view(-1, 1, 1, 1)
+                
+                sigma_x0 = self.sigma_x0  #0.0001
+                noise_x0 = torch.randn_like(mu)
+                noise_xt = torch.randn_like(mu)
 
-            x0_pred = mu + sigma_x0*noise_x0
-            xt = alpha_t.sqrt() * x0_pred + (1 - alpha_t).sqrt() * noise_xt
-            
-            #scale = 0.0
-            c1 = ((1 - alpha_t / alpha_s) * (1 - alpha_s) / (1 - alpha_t)).sqrt() * self.eta
-            c2 = ((1 - alpha_s) - c1 ** 2).sqrt()
-            #xt = xt.clone().to('cuda').requires_grad_(True)
-            if self.cond_awd:
-                scale = alpha_s.sqrt() / (alpha_s.sqrt() - c2 * alpha_t.sqrt() / (1 - alpha_t).sqrt())
-                scale = scale.view(-1)[0].item()
-            else:
-                scale = 1.0
-                        
-            et, x0_hat = self.model(xt, y, t, scale=scale)   #et, x0_pred
-            
-            if not self.awd:
-                et = (xt - x0_hat * alpha_t.sqrt()) / (1 - alpha_t).sqrt()
-            et = et.detach()
-            
-            e_obs = y_0 - H.H(x0_pred)
-            loss_obs = (e_obs**2).mean()/2
-            loss_noise = torch.mul((et - noise_xt).detach(), x0_pred).mean()
-            
-            snr_inv = (1-alpha_t[0]).sqrt()/alpha_t[0].sqrt()  #1d torch tensor
-            
-            if self.denoise_term_weight == "linear":
-                snr_inv = snr_inv
-            elif self.denoise_term_weight == "sqrt":
-                snr_inv = torch.sqrt(snr_inv)
-            elif self.denoise_term_weight == "square":
-                snr_inv = torch.square(snr_inv)
-            elif self.denoise_term_weight == "log":
-                snr_inv = torch.log(snr_inv + 1.0)
-            elif self.denoise_term_weight == "trunc_linear":
-                snr_inv = torch.clip(snr_inv, max=1.0)
-            elif self.denoise_term_weight == "power2over3":
-                snr_inv = torch.pow(snr_inv, 2/3)
-            elif self.denoise_term_weight == "const":
-                snr_inv = torch.pow(snr_inv, 0.0)
-            
-            
-            w_t = self.grad_term_weight*snr_inv   #0.25
-            v_t = self.obs_weight
+                x0_pred = mu + sigma_x0*noise_x0
+                xt = alpha_t.sqrt() * x0_pred + (1 - alpha_t).sqrt() * noise_xt
+                
+                #scale = 0.0
+                c1 = ((1 - alpha_t / alpha_s) * (1 - alpha_s) / (1 - alpha_t)).sqrt() * self.eta
+                c2 = ((1 - alpha_s) - c1 ** 2).sqrt()
+                #xt = xt.clone().to('cuda').requires_grad_(True)
+                if self.cond_awd:
+                    scale = alpha_s.sqrt() / (alpha_s.sqrt() - c2 * alpha_t.sqrt() / (1 - alpha_t).sqrt())
+                    scale = scale.view(-1)[0].item()
+                else:
+                    scale = 1.0
+                            
+                et, x0_hat = self.model(xt, y, t, scale=scale)   #et, x0_pred
+                
+                if not self.awd:
+                    et = (xt - x0_hat * alpha_t.sqrt()) / (1 - alpha_t).sqrt()
+                et = et.detach()
+                
+                e_obs = y_0 - H.H(x0_pred)
+                loss_obs = (e_obs**2).mean()/2
+                loss_noise = torch.mul((et - noise_xt).detach(), x0_pred).mean()
+                
+                snr_inv = (1-alpha_t[0]).sqrt()/alpha_t[0].sqrt()  #1d torch tensor
+                
+                if self.denoise_term_weight == "linear":
+                    snr_inv = snr_inv
+                elif self.denoise_term_weight == "sqrt":
+                    snr_inv = torch.sqrt(snr_inv)
+                elif self.denoise_term_weight == "square":
+                    snr_inv = torch.square(snr_inv)
+                elif self.denoise_term_weight == "log":
+                    snr_inv = torch.log(snr_inv + 1.0)
+                elif self.denoise_term_weight == "trunc_linear":
+                    snr_inv = torch.clip(snr_inv, max=1.0)
+                elif self.denoise_term_weight == "power2over3":
+                    snr_inv = torch.pow(snr_inv, 2/3)
+                elif self.denoise_term_weight == "const":
+                    snr_inv = torch.pow(snr_inv, 0.0)
+                
+                
+                w_t = self.grad_term_weight*snr_inv   #0.25
+                v_t = self.obs_weight
 
-            loss = w_t*loss_noise + v_t*loss_obs
-            
-            #adam step
-            optimizer.zero_grad()  #initialize
-            loss.backward()
-            optimizer.step()
-            
-            # #save for visualization
-            if self.cfg.exp.save_evolution:
-                if (ti/((self.cfg.exp.start_step - self.cfg.exp.end_step)//len(ts))) % (len(ts)//10) == 0:
-                    mu_s = torch.cat((mu_s, mu.detach().cpu()), dim=3)
-                    mu_fft_abs_s = torch.cat((mu_fft_abs_s, torch.fft.fftshift(torch.abs(torch.fft.fft2(mu.detach().cpu())))), dim=3)
-                    mu_fft_ang_s = torch.cat((mu_fft_ang_s, torch.fft.fftshift(torch.angle(torch.fft.fft2(mu.detach().cpu())))), dim=3)
-                    x0_pred_s = torch.cat((x0_pred_s, x0_pred.detach().cpu()), dim=3)
+                loss = w_t*loss_noise + v_t*loss_obs
+                
+                #adam step
+                optimizer.zero_grad()  #initialize
+                loss.backward()
+                optimizer.step()
+                
+                # #save for visualization
+                if self.cfg.exp.save_evolution:
+                    if (ti/((self.cfg.exp.start_step - self.cfg.exp.end_step)//len(ts))) % (len(ts)//10) == 0:
+                        mu_s = torch.cat((mu_s, mu.detach().cpu()), dim=3)
+                        mu_fft_abs_s = torch.cat((mu_fft_abs_s, torch.fft.fftshift(torch.abs(torch.fft.fft2(mu.detach().cpu())))), dim=3)
+                        mu_fft_ang_s = torch.cat((mu_fft_ang_s, torch.fft.fftshift(torch.angle(torch.fft.fft2(mu.detach().cpu())))), dim=3)
+                        x0_pred_s = torch.cat((x0_pred_s, x0_pred.detach().cpu()), dim=3)
                 
         if self.cfg.exp.save_evolution:
             return x0_pred, mu, mu_s, x0_pred_s, mu_fft_abs_s, mu_fft_ang_s

single_inf.py
@@ -37,10 +37,11 @@ def main(cfg):
     if classifier is not None:
         classifier.eval()
     diffusion = Diffusion(**cfg.diffusion)
-    cfg.algo.deg = "deblur_gauss"
+    cfg.algo.deg = "diffuser_cam"
     cfg.exp.num_steps = 1000
-    cfg.algo.grad_term_weight = 0.25                #lambda
-    cfg.algo.lr = 0.2
+    cfg.algo.grad_term_weight = 0.25           #lambda
+    cfg.algo.sigma_y = 0.05
+    cfg.algo.lr = 0.1
     cg_model = ClassifierGuidanceModel(model, classifier, diffusion, cfg)   #?? what is the easiest way to call stable diffusion?
 
     algo = build_algo(cg_model, cfg)
@@ -63,6 +64,7 @@ def main(cfg):
     y_0 = H.H(x)
     y_0 = y_0 + torch.randn_like(y_0) * cfg.algo.sigma_y * 2
     kwargs["y_0"] = y_0
+    kwargs["num_loop"] = 1
     yo = torch.reshape(y_0, (1, 3, 256, 256))
     yo = postprocess(yo).detach().cpu()
     img = tvu.make_grid(yo, nrow=1).permute(1, 2, 0).numpy()
@@ -78,7 +80,7 @@ def main(cfg):
     xo = postprocess(xt_s).detach().cpu()
     # convert tensor to img
     img = tvu.make_grid(xo, nrow=1).permute(1, 2, 0).numpy()
-    img = (img/img.max()* 255).astype(np.uint8)
+    img = ((img-img.min())/img.max()* 255).astype(np.uint8)
     img = Image.fromarray(img)
     # save the image
     img.save('_exp/00003_sampled.png')